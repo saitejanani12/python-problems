da203dfc6b64cbce3fa321058cd89dc18a110b27
saiteja <saitejaguntupalli296@gmail.com>
2024-01-11 00:09:50 +0530

    first commit

---
+++ prob.py
@@ -0,0 +1,7 @@
+#0.FINDING INDEX VALUES OF LIST WITHOUT ENUMERATE FUNCTIO
+#------------------------------------------------------------
+a = [1,10,12,13,10,19,10]
+sum = 0
+for x in a: 
+    sum+=1
+    print(sum-1) #finding index values without enumerate function()
